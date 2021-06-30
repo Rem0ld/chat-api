7a19c1ab8a48256e8eda1ba554dd44f21c3792dc
Rem0ld <p.lovergne@hotmail.fr>
2021-06-30 09:35:51 +0200

    cleaning stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,12 @@
 import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Number.POSITIVE_INFINITY,
-      retry: 1,
-    },
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </React.StrictMode>,
   document.querySelector("#root")
 );
