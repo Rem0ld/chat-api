ea8b163ba10d03beb6a16a20b2d7466d65c2020d
Rem0ld <p.lovergne@hotmail.fr>
2021-07-26 17:20:12 +0200

    take back div wrapper

--- src/components/Disconnect.tsx
+++ src/components/Disconnect.tsx
@@ -5,19 +5,17 @@ import { User } from "types";
 export default function Disconnect({ user }: { user: User }): ReactElement {
   const auth = useAuth();
   return (
-    <div className="h-screen w-1/4 bg-tertiary text-white font-semibold">
-      <div className="flex justify-between items-center h-auto p-2">
-        {user && user.username}
-        <button
-          className="py-2 px-1 text-gray-700 rounded-md shadow-md bg-primary"
-          type="button"
-          onClick={() => {
-            auth.signout();
-          }}
-        >
-          Disconnect
-        </button>
-      </div>
+    <div className="flex justify-between items-center h-auto py-2">
+      {user && user.username}
+      <button
+        className="py-2 px-1 text-gray-700 rounded-md shadow-md bg-primary"
+        type="button"
+        onClick={() => {
+          auth.signout();
+        }}
+      >
+        Disconnect
+      </button>
     </div>
   );
 }
